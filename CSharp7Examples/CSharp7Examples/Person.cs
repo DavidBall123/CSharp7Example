f5e85525796c469cb8d8a7df7ebcc6df2dfe26fa
David Ball <david_ball01@hotmail.com>
2017-05-07 19:06:47 +1000

    Initial project check in.

---
+++ CSharp7Examples/CSharp7Examples/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Seterlund.CodeGuard;
+
+namespace CSharp7Examples
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            Guard.That(firstName).IsNotNullOrEmpty();
+            Guard.That(firstName).IsNotNullOrEmpty();
+            Guard.That(age).IsInRange(0, 150);
+
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+    }
+}
